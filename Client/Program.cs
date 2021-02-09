60b0ca6627765510291e4933e0cc45d5a885f411
HSNHK <hsnmm1379@gmail.com>
2021-02-09 18:25:24 +0330

    create sdk and cli interface

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,23 +1,71 @@
 ﻿using System;
 using Grpc.Net.Client;
 using SystemStatusService;
-
+using Newtonsoft.Json;
 namespace Client
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Environment.GetEnvironmentVariable("AUTHTOKEN"));
-            using var Channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var greeterClient = new Greeter.GreeterClient(Channel);
-            var res = greeterClient.info(new info_request
+            Console.WriteLine("welcome to server status service");
+            Console.Write("Enter server addres : ");
+            string address = Console.ReadLine();
+            Console.Write("Enter server token :");
+            string token = Console.ReadLine();
+            Console.Clear();
+            sdk SystemStatus = new sdk(address, token);
+            Console.WriteLine("1>Info\n2>process\n3>command\n0>exit");
+            while (true)
             {
-                Token = "123456"
-            });
-
-            Console.WriteLine(res.Message);
-            Console.ReadKey();
+                Console.Write("Command :~>#");
+                int command =int.Parse(Console.ReadLine());
+                if (command == 1)
+                {
+                    foreach (var item in SystemStatus.Info())
+                    {
+                        Console.WriteLine($"{item.Key} : {item.Value}");
+                    }
+                }
+                else if (command==2)
+                {
+                    foreach (var items in SystemStatus.Process())
+                    {
+                        foreach (var item in items)
+                        {
+                            Console.WriteLine($"ID : {item.Key} | Name : {item.Value}");
+                        }
+                    }
+                }
+                else if (command==3)
+                {
+                    while (true)
+                    {
+                        Console.Write($"{token} #enter command >");
+                        string cmd = Console.ReadLine();
+                        if (cmd == "exit")
+                        {
+                            break;
+                        }
+                        else if (!string.IsNullOrEmpty(cmd))
+                        {
+                            foreach (var item in SystemStatus.Command(cmd))
+                            {
+                                Console.WriteLine(item.Value);
+                            }
+                        }
+                    }
+                }
+                else if (command==0)
+                {
+                    Console.WriteLine("Goodbye...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"'{command}' is not recognized as an internal or external command");
+                }
+            }
         }
     }
 }
---
+++ Client/sdk.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Grpc.Net.Client;
+using SystemStatusService;
+using Newtonsoft.Json;
+namespace Client
+{
+    class sdk
+    {
+        private string Token { get; set; }
+        private string Address { get; set; }
+        private GrpcChannel Channel;
+        private Greeter.GreeterClient Client;
+        public sdk(string Address, string Token)
+        {
+            this.Address = Address;
+            this.Token = Token;
+            this.Channel = GrpcChannel.ForAddress(Address);
+            this.Client = new Greeter.GreeterClient(this.Channel);
+        }
+
+        public Dictionary<string,string> Info()
+        {
+            var response = this.Client.info(new info_request
+            {
+                Token = this.Token
+            });
+            return JsonConvert.DeserializeObject<Dictionary<string,string>>(response.Message);
+        }
+
+        public List<Dictionary<int, string>> Process()
+        {
+            var response = this.Client.process(new process_request
+            {
+                Token = this.Token
+            });
+            return JsonConvert.DeserializeObject<List<Dictionary<int, string>>>(response.Message);
+        }
+
+        public Dictionary<string,string> Command(string cmd)
+        {
+            var response = this.Client.command(new command_request
+            {
+                Token = this.Token,
+                Cmd = cmd
+            });
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(response.Message);
+        }
+    }
+}
