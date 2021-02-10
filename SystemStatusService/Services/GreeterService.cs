4d9d9dad2633edd3e21fac526838ab21d6bb35e6
HSNHK <hsnmm1379@gmail.com>
2021-02-10 13:07:31 +0330

    All functions were created and registered for the service

--- SystemStatusService/Services/GreeterService.cs
+++ SystemStatusService/Services/GreeterService.cs
@@ -1,12 +1,10 @@
+using Core;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Grpc.Core;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using Core;
-using Newtonsoft.Json;
 
 namespace SystemStatusService
 {
@@ -14,31 +12,94 @@ namespace SystemStatusService
     {
 
         private readonly ILogger<GreeterService> _logger;
-        private SystemInformation SysInfo;
+        private SystemInformation SystemInfo;
         private readonly string Token;
         public GreeterService(ILogger<GreeterService> logger)
         {
             _logger = logger;
             Token = Environment.GetEnvironmentVariable("AUTHTOKEN");
-            SysInfo = new SystemInformation();
+            SystemInfo = new SystemInformation();
         }
 
-        public override Task<response> info(info_request request, ServerCallContext context)
+        public override Task<response> login(login_request request, ServerCallContext context)
         {
-            if (request.Token == Token)
+            if (request.Token==Token)
             {
                 return Task.FromResult(new response
                 {
                     Status = true,
-                    Message = JsonConvert.SerializeObject(SysInfo.Info())
+                    Message = Environment.UserName
                 });
             }
             else
             {
                 return Task.FromResult(new response
                 {
-                    Status=false,
-                    Message = "authentication error"
+                    Status = false,
+                    Message = "Authentication was not successful"
+                });
+            }
+        }
+
+        public override Task<response> info(login_request request, ServerCallContext context)
+        {
+            if (request.Token == Token)
+            {
+                _logger.LogInformation("A request for info was sent");
+                return Task.FromResult(new response
+                {
+                    Status = true,
+                    Message = JsonConvert.SerializeObject(SystemInfo.Info())
+                });
+            }
+            else
+            {
+                return Task.FromResult(new response
+                {
+                    Status = false,
+                    Message = "Authentication was not successful"
+                });
+            }
+        }
+
+        public override Task<response> process(login_request request, ServerCallContext context)
+        {
+            if (request.Token == Token)
+            {
+                _logger.LogInformation("A request for process was sent");
+                return Task.FromResult(new response
+                {
+                    Status = true,
+                    Message = JsonConvert.SerializeObject(SystemInfo.SystemProcess())
+                });
+            }
+            else
+            {
+                return Task.FromResult(new response
+                {
+                    Status = false,
+                    Message ="Authentication was not successful"
+                });
+            }
+        }
+
+        public override Task<response> command(command_request request, ServerCallContext context)
+        {
+            if (request.Token == Token)
+            {
+                _logger.LogInformation($"A request for command was sent : {request.Cmd}");
+                return Task.FromResult(new response
+                {
+                    Status = true,
+                    Message = JsonConvert.SerializeObject(SystemInfo.Command(request.Cmd))
+                });
+            }
+            else
+            {
+                return Task.FromResult(new response
+                {
+                    Status = false,
+                    Message ="Authentication was not successful"
                 });
             }
         }
